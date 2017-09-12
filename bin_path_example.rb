89f353c63ff6b519f928aec5f7381cb3016139ea
Patricia Arbona <arbonap@gmail.com>
2017-09-12 12:52:45 -0700

    Notes about pathname class

--- bin_path_example.rb
+++ bin_path_example.rb
@@ -9,3 +9,41 @@ $ ruby -rbundler/setup -e 'puts Gem.bin_path("bundler", "bundler"); puts ENV["PA
 
 # use magical comment in Ruby < 2 to handle utf-8/unicode : # encoding: utf-8
 # ruby < 2 defaulted to interpreting source as ASCII, ruby 2+ default to utf8. Ruby < 2 handled unicode just fine, it just didn't default to it
+
+# ls source/**/*.md - recursively search through file system for files ending with markdown extension within the source directory
+# Pathname library:
+path.directory?
+path.exist?
+path.readable?
+path.writable?
+
+# Let’s imagine that you have the following folder structure:
+#
+# lib/
+#   + script.rb
+# data/
+#   + file.txt
+# You want to access file.txt from your script.rb script,
+# but you want to make sure that this works whatever working directory you
+# run the script from. That means you need to figure out what the
+# absolute path to file.txt is, and then reference it using this absolute path.
+
+
+pn = Pathname.new('/usr/bin/ruby')
+pn.file?       # => true
+pn.directory?  # => false
+
+pn.absolute?   # => true
+pn.relative?   # => false
+
+pn.executable? # => true
+pn.readable?   # => true
+pn.writable?   # => false
+
+pn.root?       # => false
+
+Pathname.glob('**/*.rb') # like Dir['**/*.rb'] but returns Pathnames instead of files strings (?maybe i think?)
+
+# path = Pathname(__FILE__).dirname.parent + "data" + "some_file"
+
+# $1, $2, ...	The match for the first, second, etc. parenthesized groups in the last regular expression.
